8b94380bba992abad856b2e67f155769cfe5d7e0
Artem Vardazarov <vardazarov01@mail.ru>
2025-04-07 19:38:29 +0300

    corps

--- src/main.mjs
+++ src/main.mjs
@@ -39,10 +39,24 @@ app.use(
 )
 app.use(passport.initialize())
 app.use(passport.session())
-app.use(cors({
-    origin: 'http://109.73.205.115:3000',
-    credentials: true,
-}));
+
+const allowedOrigins = [
+    'http://109.73.205.115:33305',
+    'http://109.73.205.115:3000',
+    'http://localhost:3000'  
+];
+  
+const corsOptions = {
+    origin: function(origin, callback) {
+      if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS')); 
+      }
+    }
+};
+  
+app.use(cors(corsOptions));
 
 app.use(usersRouter)
 app.use(ordersRouter)
